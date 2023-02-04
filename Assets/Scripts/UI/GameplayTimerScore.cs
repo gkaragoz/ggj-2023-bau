70f880e8b19b5043a421ce6620964087ba5dfc3e
gkaragoz <gkaragoz96@gmail.com>
2023-02-05 00:26:15 +0300

    Create GameplayTimeScore script and implement it.

---
+++ Assets/Scripts/UI/GameplayTimerScore.cs
@@ -0,0 +1,45 @@
+﻿using Gameplay;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class GameplayTimerScore : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI ScoreText;
+        [SerializeField] private TextMeshProUGUI TimerText;
+        
+        private void OnEnable()
+        {
+            GameManager.OnStart += OnGameStarted;
+            TimerManager.OnTimeChangeAsFormattedString += OnTimeChangeAsFormattedString;
+        }
+
+        private void OnDisable()
+        {
+            GameManager.OnStart -= OnGameStarted;
+            TimerManager.OnTimeChangeAsFormattedString -= OnTimeChangeAsFormattedString;
+        }
+
+        private void SetScore(int score)
+        {
+            ScoreText.text = $"SCORE: <color=#A0D235>{score}</color>";
+        }
+        
+        private void SetTimerText(string timerText)
+        {
+            TimerText.text = timerText;
+        }
+
+        private void OnTimeChangeAsFormattedString(string formattedTime)
+        {
+            SetTimerText(formattedTime);
+        }
+
+        private void OnGameStarted()
+        {
+            SetScore(0);
+            SetTimerText("00:00:00");
+        }
+    }
+}
