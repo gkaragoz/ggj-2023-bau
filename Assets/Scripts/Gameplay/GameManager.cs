537248b78389c6e44ab58c8134af5e19b1212456
Hasan Tunç <hasantunc.dev@gmail.com>
2023-02-04 16:42:29 +0300

    Completes Wave system

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -6,6 +6,12 @@ namespace Gameplay
     public class GameManager : Singleton<GameManager>
     {
         public static Action OnStart;
+        public static Action OnPause;
         public static Action OnComplete;
+
+        private void Start()
+        {
+            OnStart?.Invoke();
+        }
     }
 }
