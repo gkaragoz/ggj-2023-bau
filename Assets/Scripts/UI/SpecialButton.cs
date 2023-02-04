19f26a4eb7e7e7bb13caef85d69c813dc984a478
gkaragoz <gkaragoz96@gmail.com>
2023-02-04 23:39:04 +0300

    Implement new Onion and Potato to Main Canvas.

---
+++ Assets/Scripts/UI/SpecialButton.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class SpecialButton : Button
+    {
+        [SerializeField] private TextMeshProUGUI Text;
+        private readonly Color32 _normalColor = new Color32(56, 113, 3, 255);
+        private readonly Color32 _highlightedColor = new Color32(244, 245, 209, 255);
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            Text = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            base.DoStateTransition(state, instant);
+            
+            if (Text == null) return;
+            
+            switch (state)
+            {
+                case SelectionState.Normal:
+                case SelectionState.Selected:
+                case SelectionState.Disabled:
+                    Text.color = _normalColor;
+                    break;
+                case SelectionState.Highlighted:
+                case SelectionState.Pressed:
+                    Text.color = _highlightedColor;
+                    break;
+                default:
+                    Text.color = _normalColor;
+                    break;
+            }
+        }
+    }
+}
